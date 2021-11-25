8c338b1011be6ff71772b089b5550f85abae07dc
Jason van Gumster <fweeb@monsterjavaguns.com>
2021-11-24 21:40:37 -0500

    Commented out the brute force bounce through SVG

--- playground.py
+++ playground.py
@@ -4,28 +4,7 @@ import os
 
 # Local imports
 from . import global_vars
-
-
-def hex_to_rgb(value):
-    gamma = 2.2
-    value = value.lstrip('#')
-    lv = len(value)
-    fin = list(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-    r = pow(fin[1] / 255, gamma)
-    g = pow(fin[2] / 255, gamma)
-    b = pow(fin[0] / 255, gamma)
-    fin.clear()
-    fin.append(r)
-    fin.append(g)
-    fin.append(b)
-    return tuple(fin)
-
-
-def hex_to_rgba(value):
-    rgb = hex_to_rgb(value)
-    rgba = list(rgb)
-    rgba.append(1.0) # Just set alpha to 1
-    return tuple(rgba)
+from .lib.world_env import build_world
 
 
 class SWF_OT_test_operator(bpy.types.Operator):
@@ -36,27 +15,13 @@ class SWF_OT_test_operator(bpy.types.Operator):
 
     def execute(self, context):
         from .lib.swf.movie import SWF
-        from .lib.swf.tag import TagSetBackgroundColor
-        from .lib.swf.utils import ColorUtils
         testfile = open(os.path.abspath(os.path.dirname(__file__) + "/test/bumble-bee1.swf"), "rb")
         testswf = SWF(testfile)
         print(testswf)
-        # Divide by 20 because SWF units are in "twips", 1/20 of a pixel
-        width = (testswf.header.frame_size.xmax - testswf.header.frame_size.xmin) / 20
-        height = (testswf.header.frame_size.ymax - testswf.header.frame_size.ymin) / 20
 
-        # Background color (loops should result in just one tag)
-        bg_tag = [x for x in testswf.all_tags_of_type(TagSetBackgroundColor)]
+        build_world(testswf)
 
-        bpy.context.scene.render.resolution_x = width
-        bpy.context.scene.render.resolution_y = height
-        bpy.context.scene.render.fps = testswf.header.frame_rate
-        bpy.context.scene.frame_end = testswf.header.frame_count
-        bpy.context.scene.world.color = hex_to_rgb(ColorUtils.to_rgb_string(bg_tag[0].color)) #XXX to_rgb_string seems to return brg instead of rgb
-        bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = hex_to_rgba(ColorUtils.to_rgb_string(bg_tag[0].color))
-
-        print(len(testswf.tags))
-
+        '''
         # Here's a dumb idea... use pyswf's built-in convert to SVG function... then import that using Blender's SVG importer
         from .lib.swf.export import SVGExporter
         svg_exporter = SVGExporter()
@@ -67,6 +32,7 @@ class SWF_OT_test_operator(bpy.types.Operator):
         open(tmpout_path, "wb").write(testsvg.read())
         #bpy.ops.wm.gpencil_import_svg(filepath=tmpout_path, scale=100, resolution=100) #XXX This seems so not be functional
         bpy.ops.import_curve.svg(filepath=tmpout_path) # Then convert to Grease Pencil. Not ideal, but it kind of works
+        '''
 
         return {"FINISHED"}
 
