1d941983d8e2e9b01267f71c99b02a9e6c7c67d8
Jason van Gumster <fweeb@monsterjavaguns.com>
2021-11-29 17:25:58 -0500

    We're drawing! Not at all accurate. But we're drawing!

--- lib/world_env.py
+++ lib/world_env.py
@@ -2,6 +2,7 @@ import bpy
 
 
 # Local imports
+from .globals import *
 from .swf.tag import TagSetBackgroundColor
 from .swf.utils import ColorUtils
 
@@ -30,11 +31,15 @@ def hex_to_rgba(value):
 
 def build_world(swfdata):
     # Divide by 20 because SWF units are in "twips", 1/20 of a pixel
-    width = (swfdata.header.frame_size.xmax - swfdata.header.frame_size.xmin) / 20
-    height = (swfdata.header.frame_size.ymax - swfdata.header.frame_size.ymin) / 20
+    width = (swfdata.header.frame_size.xmax - swfdata.header.frame_size.xmin) / PIXELS_PER_TWIP
+    height = (swfdata.header.frame_size.ymax - swfdata.header.frame_size.ymin) / PIXELS_PER_TWIP
 
     # Background color (loops should result in just one tag)
     bg_tag = [x for x in swfdata.all_tags_of_type(TagSetBackgroundColor)]
+
+    #XXX Assume an orthographic camera because taking perspective into account when converting pixels to real units is hard
+    bpy.context.scene.camera.data.type = "ORTHO"
+    bpy.context.scene.camera.data.ortho_scale = max([width, height]) / PIXELS_PER_METER
 
     bpy.context.scene.render.resolution_x = width
     bpy.context.scene.render.resolution_y = height
@@ -43,4 +48,4 @@ def build_world(swfdata):
     bpy.context.scene.world.color = hex_to_rgb(ColorUtils.to_rgb_string(bg_tag[0].color)) #XXX to_rgb_string seems to return brg instead of rgb
     bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = hex_to_rgba(ColorUtils.to_rgb_string(bg_tag[0].color))
 
-    print(len(swfdata.tags))
+    #print(len(swfdata.tags))
--- playground.py
+++ playground.py
@@ -1,10 +1,13 @@
 import bpy
 import os
+from math import sin
 
 
 # Local imports
 from . import global_vars
-from .lib.world_env import build_world
+from .lib.globals import *
+from .lib.world_env import build_world, hex_to_rgba
+from .lib.swf.utils import ColorUtils
 
 
 class SWF_OT_test_operator(bpy.types.Operator):
@@ -17,7 +20,7 @@ class SWF_OT_test_operator(bpy.types.Operator):
         from .lib.swf.movie import SWF
         testfile = open(os.path.abspath(os.path.dirname(__file__) + "/test/bumble-bee1.swf"), "rb")
         testswf = SWF(testfile)
-        print(testswf)
+        #print(testswf)
 
         build_world(testswf)
 
@@ -33,14 +36,105 @@ class SWF_OT_test_operator(bpy.types.Operator):
         #        * There's a readstyle_array_length subfunction... not sure what it does.
         #    * The PlaceObject and PlaceObject2 tags (types 4 and 26, respectively) tell how and where assets are moved, based on their characterId (defined in the Define[blah] tag)
 
+        orig_frame = bpy.context.scene.frame_current
         bpy.context.scene.frame_current = 1
 
         for tag in testswf.tags:
-            if "DefineShape" in tag.name: # We have a new object to add!
-                #XXX For now, assumes DefineShape4
+            if tag.name.startswith("DefineShape"): # We have a new object to add!
+                # Make a new Grease Pencil object to hold our shapes
+                gp_data = bpy.data.grease_pencils.new(tag.name + ".{0:03}".format(tag.characterId))
+                gp_object = bpy.data.objects.new(tag.name + ".{0:03}".format(tag.characterId), gp_data)
+                gp_object["swf_characterId"] = tag.characterId
+                # Define initial fill and line materials
+                for fill_style in tag.shapes._initialFillStyles:
+                    gp_mat = bpy.data.materials.new("FillStyle")
+                    bpy.data.materials.create_gpencil_data(gp_mat)
+                    gp_mat.grease_pencil.show_stroke = False
+                    gp_mat.grease_pencil.show_fill = True
+                    gp_mat.grease_pencil.fill_color = hex_to_rgba(ColorUtils.to_rgb_string(fill_style.rgb))
+                    if fill_style.type == 0:
+                        gp_mat.grease_pencil.fill_style = "SOLID"
+                    else:
+                        raise Exception("Non-solid fill types are currently not supported")
+                    gp_data.materials.append(gp_mat)
+                for line_style in tag.shapes._initialLineStyles:
+                    gp_mat = bpy.data.materials.new("LineStyle")
+                    bpy.data.materials.create_gpencil_data(gp_mat)
+                    gp_mat.grease_pencil.show_stroke = True
+                    if line_style.fill_type == None:
+                        gp_mat.grease_pencil.show_fill = False
+                    else:
+                        raise Exception("Filled line styles not currently supported")
+                    gp_mat.grease_pencil.color  = hex_to_rgba(ColorUtils.to_rgb_string(line_style.color))
+                    gp_mat["swf_linewidth"] = line_style.width
+                    gp_mat["swf_line_miter"] = 3.0
+                    gp_data.materials.append(gp_mat)
+
+                # We need some basic layer stuff in our Grease Pencil object for drawing
+                gp_layer = gp_data.layers.new("Layer", set_active = True)
+                gp_frame = gp_layer.frames.new(bpy.context.scene.frame_current) #XXX This is only the first frame... not all of them
+                # Start creating shapes
+                draw_pos = [0.0, 0.0] #XXX Should start as the object/shape origin
+                for shape in tag.shapes.records:
+                    if shape.type == 1: # EndShapeRecord... this should be the last shape record
+                        break
+                    elif shape.type == 2: # StyleChangeRecord
+                        if shape.state_moveto:
+                            move_x = shape.move_deltaX / PIXELS_PER_TWIP / PIXELS_PER_METER
+                            move_y = shape.move_deltaY / PIXELS_PER_TWIP / PIXELS_PER_METER
+                            draw_pos = [move_x, move_y]
+                        #XXX Still need to handle fill and line style selection
+                    elif shape.type == 4: # CurvedEdgeRecord
+                        gp_stroke = gp_frame.strokes.new()
+                        gp_stroke.line_width = 300 #XXX placeholder
+                        gp_stroke.display_mode = "3DSPACE"
+                        gp_stroke.material_index = 0 #XXX placeholder
+                        anchor1 = draw_pos
+                        control = [draw_pos[0] + (shape.control_deltaX / PIXELS_PER_TWIP / PIXELS_PER_METER),
+                                   draw_pos[1] + (shape.control_deltaY / PIXELS_PER_TWIP / PIXELS_PER_METER)]
+                        anchor2 = [control[0] + (shape.anchor_deltaX / PIXELS_PER_TWIP / PIXELS_PER_METER),
+                                   control[1] + (shape.anchor_deltaY / PIXELS_PER_TWIP / PIXELS_PER_METER)]
+                        gp_points = [anchor1, control, anchor1]
+                        for point in gp_points:
+                            gp_stroke.points.add(1)
+                            gp_stroke.points[-1].co.x = point[0]
+                            gp_stroke.points[-1].co.y = point[1]
+
+            if tag.name.startswith("PlaceObject"):
+                if tag.hasCharacter and not tag.hasMove:
+                    # Add a new character (that we've already defined with ID of characterId)
+                    # Find the object we already created with the identified character ID
+                    ob_is_found = False
+                    for ob in bpy.data.objects:
+                        if "swf_characterId" in ob.keys() and ob["swf_characterId"] == tag.characterId:
+                            bpy.context.collection.objects.link(ob)
+                            if tag.hasMatrix:
+                                #XXX Blender doesn't support shearing at the object level, so the rotateSkew0 and rotateSkew1 values can only be used for rotation
+                                #XXX Also, Blender's 2D Animation workspace defaults to the camera looking down the Y axis... so xy in SWF is xz in Blender
+                                translate_x = tag.matrix.translateX / PIXELS_PER_TWIP / PIXELS_PER_METER
+                                translate_z = tag.matrix.translateY / PIXELS_PER_TWIP / PIXELS_PER_METER
+                                rotation_y = sin(tag.matrix.rotateSkew0)
+                                ob.location = (ob.location[0] + translate_x, ob.location[1], ob.location[2] + translate_z)
+                                ob.rotation_euler[1] += rotation_y
+                                ob.scale = (tag.matrix.scaleX, ob.scale[1], tag.matrix.scaleY)
+
+                            ob["swf_depth"] = tag.depth
+                            ob_is_found = True
+                            break
+                    if not ob_is_found:
+                        raise Exception("Trying to place an object/characterId that hasn't been defined.")
+                #if not tag.hasCharacter and tag.hasMove:
+                    # Character at given depth (only one character is allowed at a given depth) has been modified
+                    #break
+                #if tag.hasCharacter and tag.hasMove:
+                    # Character at given depth is removed. New character (already defined with ID of characterId) is added at given depth
+                    #break
 
             if tag.name == "ShowFrame":
                 bpy.context.scene.frame_current += 1
+
+        bpy.context.scene.frame_end = bpy.context.scene.frame_current - 1
+        bpy.context.scene.frame_current = orig_frame
 
         '''
         # Here's a dumb idea... use pyswf's built-in convert to SVG function... then import that using Blender's SVG importer
